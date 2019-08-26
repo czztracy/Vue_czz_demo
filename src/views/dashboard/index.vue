f620336ccd870417eca78423eaa8f4fef91ddf47
czztracy <879742352@qq.com>
2019-08-26 10:07:56 +0800

    mapActions等辅助函数

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -3,7 +3,21 @@
 </template>
 
 <script>
+// import { mapActions, mapGetters, mapState } from 'vuex'
 export default {
-  name: 'dashboard'
+  name: 'dashboard',
+  methods: {
+    // ...mapActions(['GET_ISCOLLAPSE'])
+  },
+  computed: {
+    // ...mapGetters(['isCollapse'])
+  },
+  mounted () {
+    // this.GET_ISCOLLAPSE(false)
+    // .then(res => {
+    //   console.log('ewa', res)
+    // })
+    // console.log('isCollapse', this.isCollapse)
+  }
 }
 </script>
